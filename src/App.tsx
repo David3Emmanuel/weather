7d196b7bb047f781b747ba95d1dd094d4158ff04
David Emmanuel <david3emmanuel@gmail.com>
2024-08-17 13:27:49 +0100

    Connected to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 import countriesMap from './api/countries.json'
+import { fetchWeatherData } from './api/country';
+import ReportData from './components/ReportData';
 
 function App() {
   const countries = Object.keys(countriesMap);
-  const [country, setCountry] = useState(countries[0]);
+  const [country, setCountry] = useState(countries[0] as keyof typeof countriesMap);
+  const [report, setReport] = useState<Object | null>(null);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
+    setReport(null);
+    const { lat, long } = countriesMap[country];
+    const data = await fetchWeatherData(lat, long);
+    console.log(data);
+    setReport(data);
   }
 
   return <div className='app'>
-    <h1>Weather</h1>
+    <h2>Weather Report</h2>
     <form onSubmit={handleSubmit}>
-      <select value={country} onChange={e => setCountry(e.target.value)}>
-        {countries.map((country, i) => <option key={i} value={country}>{country}</option> )}
+      <select value={country} onChange={e => setCountry(e.target.value as keyof typeof countriesMap)}>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
       </select>
       <button type='submit'>Submit</button>
     </form>
+
+    {report && Object.entries(report).map(([key, value], i) => <ReportData key={i} reportKey={key} value={value} />)}
   </div>
 }
 
--- src/api/country.ts
+++ src/api/country.ts
@@ -1,10 +1,16 @@
 const apikey = 'ZeW4R073W9kDEj98Iah8efSxMGKbJ5MS'; 
 
-const countries = async (lat: number, lon: number) => { 
+export const fetchWeatherData = async (lat: number, lon: number) => { 
     const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${lat},${lon}&apikey=${apikey}`);
     const data = await response.json();
-    return data.timelines.daily[0];
-}
+    const values = data.timelines.daily[0].values;
 
-const result = await countries(9.0820, 8.6753);
-console.log(result);
+    console.log(values);
+
+    return {
+        "Temperature": values.temperatureAvg,
+        "Humidity": values.humidityAvg,
+        "Chance of Rain": values.precipitationProbabilityAvg,
+        "Wind Speed": values.windSpeedAvg,
+    } as Object
+}

e44fd4fec8fc26085b92464fc562af7ccd103715
David Emmanuel <david3emmanuel@gmail.com>
2024-08-22 19:32:58 +0100

    styled search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -32,17 +32,34 @@ export default function Search({ submit }: {
         setSuggestions([]);
     }
 
+    const clearQuery = () => {
+        setQuery('');
+        setSuggestions([]);
+        setState(State.DONE);
+    }
+
     return <div className="search">
-        <input
-            type="text"
-            placeholder="City Name"
-            value={query}
-            onChange={handleInput}
-        />
-        {state === State.LOADING && <div className="loading">Loading...</div>}
-        {state === State.ERROR && <div className="error">Something went wrong. <a>Try again</a></div>}
-        {state === State.DONE && suggestions && <ul>
-            {suggestions.map(suggestion => <li key={suggestion.id} onClick={() => handleSubmit(suggestion)}>{suggestion.name}, {suggestion.country}</li>)}
+        <div className="search__header">
+            <span className="search__icon material-symbols-outlined">search</span>
+            <input
+                type="text"
+                placeholder="City Name"
+                value={query}
+                onChange={handleInput}
+            />
+            {query && <span className="search__clear material-symbols-outlined" onClick={clearQuery}>close</span>}
+        </div>
+        {suggestions.length > 0 && <div className="divider"></div>}
+        {state === State.LOADING && <div className="loading message">Loading...</div>}
+        {state === State.ERROR && <div className="error message">Something went wrong. <a>Try again</a></div>}
+        {state === State.DONE && suggestions.length > 0 && <ul>
+            {suggestions.map(suggestion => <li key={suggestion.id} onClick={() => handleSubmit(suggestion)}>
+                <div className="search__suggestion-overlay"></div>
+                <div className="search__suggestion-content">
+                    <div className="search__city">{suggestion.name}</div>
+                    <div className="search__country">{suggestion.country}</div>
+                </div>
+            </li>)}
         </ul>}
     </div>
 }

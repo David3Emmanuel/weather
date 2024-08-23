214de163e6459964eb524702e4309dedcc635a54
David Emmanuel <david3emmanuel@gmail.com>
2024-08-23 21:56:55 +0100

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import './Footer.css';
+
+export default function Footer() {
+    return <footer>
+        <p>By <a href="github.com/David3Emmanuel">David3Emmanuel</a> and <a href="github.com/dchefcoder">dchefcoder</a></p>
+    </footer>
+}
